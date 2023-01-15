48fdd2d40c89a198838818214174fca71d73240b
arasch13 <arasch13@web.de>
2023-01-15 13:00:52 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hi there! This is Araschs Test FE app!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
